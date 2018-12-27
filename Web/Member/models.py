737f3c82227a2d428e87d1d1dd2078f23db17b7d
weiber-hsu <39873242+weiber-hsu@users.noreply.github.com>
2018-12-28 00:17:03 +0800

    delete model

--- Web/Member/models.py
+++ Web/Member/models.py
@@ -1,27 +1,4 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class table(models.Model):
-    time=models.DateTimeField(verbose_name="time",auto_now=True)
-    NS
-    EW
-    
-class seat(models.Model):
-    position=models.CharField(max_length=1)
-    table_ID=models.ForeignKey('table',on_delete=models.CASCADE)
-    player_ID=models.ForeignKey('user',on_delete=models.DO_NOTHING)
 
-class rounds(models.Model):
-    dealer=models.CharField(max_length=2)
-    bid
-    leadplayer
-    N
-    E
-    W
-    S
-    vunerable
-    NS
-    EW
-    game_type
-    table_ID==models.ForeignKey('table',on_delete=models.CASCADE)
-    
