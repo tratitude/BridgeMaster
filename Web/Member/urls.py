958d20f7d9b963e2717cd7bc64abc5ddeddddca1
Ridost <liao60209@gmail.com>
2019-01-01 22:09:03 +0800

    01

--- Web/Member/urls.py
+++ Web/Member/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 	path('playmode=<int:pm>/',views.playmode,name = 'playmode'),
 	path('Table/',views.tableinformation,name = 'table'),
 	path('Table/<int:tid>',views.tableinformation,name = 'table'),
+	path('connect/',views.connect,name='connect'),
 #	path('GameStart/',views.GameStart,name = 'GameStart')
 	#path('modify_password/',views.modify_password),
 ]
