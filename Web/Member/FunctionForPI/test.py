6422b78978a463a2eebbc72298068e6740a84e3f
Ridost <liao60209@gmail.com>
2019-01-10 21:38:21 +0800

    mode#Classic

--- Web/Member/FunctionForPI/test.py
+++ Web/Member/FunctionForPI/test.py
@@ -1,11 +1,14 @@
-Data = {'bmbc':'2','state':'2','round':{'N':'K8732.43.K74.AK4','E':'QJ.KQT9.93.QJT75','S':'T54.J652.QJT.986','W':'A96.A87.A8652.32'},
-            'from':'15'}
-round = Data['round']
-Cards = []
-for key in round:
-    Cards.append(round[key])
 
-for i in range(4):
-    Card = Cards[i].split('.')
-    Cards[i] = Card
-print(Cards[2])
+def CardTransfer(v):  #把Barcode轉成PBN格式
+    point = {
+        '1':'A',
+        'A':'T',
+        'B':'J',
+        'C':'Q',
+        'D':'K'
+    }
+    return point[v]
+
+point  = 'C'
+point = CardTransfer(point)
+print(point)
