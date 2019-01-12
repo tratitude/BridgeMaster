18df983a2a6a792287c1d0d155419ad7cf90eed2
fdmdkw <lk1919618@gmail.com>
2019-01-12 12:03:59 +0800

    successfully runserver

---
+++ Web/DDS/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.dds,name='dds')
+]
